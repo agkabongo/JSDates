25aa6b34b58769218e396801978b64e0ceacc990
agkabongo <agkabongo@LAPTOP-AGK.localdomain>
2020-11-30 13:53:59 +0100

    exercises 1-3

--- dates.js
+++ dates.js
@@ -1,25 +1,68 @@
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-var months = ['January', 'February','March','April','May','June','July', 'August', 'September','October','November','December'];
 
-
-   
-    var x = document.createElement('div');
+let titres = ['Exercise 1', 'Exercise 2', 'Exercise 3', 'Exercise 4']
+ titres.forEach(element => {
+    let x = document.createElement('div');
     x.className = 'container'
     document.body.appendChild(x)
+    let unTitre = document.createElement('H1') 
+    let par = document.createElement('p')
+    unTitre.innerHTML = element
+    x.appendChild(unTitre)
+    x.appendChild(par)
+ });
 
 
-    var d = new Date();
-    var current = d.toLocaleString('fr-FR', {timeZone: 'Europe/Brussels'})
-     var american = d.toLocaleString('en-US', { timeZone: 'America/Anchorage' })
-     var iceland = d.toLocaleString('en-US', { timeZone : 'Iceland'})
-     
-    /*
-    var year = d.setUTCFullYear();
-    var month = months[d.setUTCMonth()];
-    var day = d.setUTCDate();
-    var hrs = d.getUTCHours();
-    var min = d.getUTCMinutes();
-    var sec = d.getUTCSeconds();*/
-    x.innerHTML = 'Brussels - ' + current + '<br>' + 'Anchorage - ' + american + '<br>' + 'Reykjavik - ' + iceland
- 
-    /*x.innerHTML = day + ' ' + month + ' '+ year + ' ' + hrs + " : " + min + " : " + sec;*/
+let paragraphe = document.querySelectorAll('p')
+
+//EXERCISE 1
+
+let d = new Date();
+let current = d.toLocaleString('fr-FR', {
+    timeZone: 'Europe/Brussels'
+})
+let american = d.toLocaleString('en-US', {
+    timeZone: 'America/Anchorage'
+})
+let iceland = d.toLocaleString('en-US', {
+    timeZone: 'Iceland'
+})
+
+//EXERCISE 2 
+let birthday = new Date()
+let today= new Date()
+birthday.setFullYear(1995, 9, 10)
+let days = (today - birthday) / (1000 * 60 * 60 * 24)
+let nbrmonths = Math.ceil(days / 31)
+let nbrYears = Math.ceil(nbrmonths / 12)
+
+
+//EXERCISE 3 
+let numberInput = document.createElement('input')
+let userInput; 
+numberInput.setAttribute('type','number')
+numberInput.setAttribute('step', '1000')
+numberInput.value = '1000'
+document.getElementsByClassName('container')[2].appendChild(numberInput)
+
+userInput = numberInput.addEventListener('keyup', timeStamps)
+
+function timeStamps(){
+    future = new Date()
+    future.setHours(numberInput.value)
+    return paragraphe[2].innerHTML = 'In ' + numberInput.value + ' hours we will be ' + future
+}
+
+//EXERCICE 4 
+
+
+
+
+
+//OUTPUT
+paragraphe[0].innerHTML = `Brussels - ${current}<br>Anchorage - ${american}<br>Reykjavik - ${iceland}`
+paragraphe[1].innerHTML = 'Between today  ' + today + ' and your birthday ' + birthday + ' We have ' + days + ' days, ' + nbrmonths + ' months, ' + nbrYears + ' years'
+paragraphe[2].innerHTML = 'We find the exact time and date we will be in 80000 hours.'
+
+/*timeZone for d.localString https://gist.github.com/diogocapela/12c6617fc87607d11fd62d2a4f42b02a*/
